cc17578ae49706fd366352b1bd4ac873c3f371f8
avielpalgi <avielpalgi@gmail.com>
2021-05-27 21:17:19 +0300

    make it better

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use('/app/protected', auth,(req,res)=>{
 if(process.env.NODE_ENV === 'production'){
 app.use(express.static('client/build'));
 app.get("/*",(req,res)=>{
-    res.sendFile(path.join(__dirname,'client','build','index.html'))
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
 })
 }
 else{
