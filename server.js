40a24b0ad30116957abc3aee39353e177e48ec37
avielpalgi <avielpalgi@gmail.com>
2021-05-27 20:46:21 +0300

    make it better

--- server.js
+++ server.js
@@ -34,12 +34,12 @@ app.use('/app/protected', auth,(req,res)=>{
 
 if(process.env.NODE_ENV === 'production'){
 app.use(express.static('client/build'));
-app.get('/',(req,res)=>{
+app.get("*",(req,res)=>{
     res.sendFile(path.resolve(__dirname,'client','build','index.html'))
 })
 }
 else{
-    app.get("*",(req,res)=>{
+    app.get('/',(req,res)=>{
         res.send('Api running');
     })
 }
